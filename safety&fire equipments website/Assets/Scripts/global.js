52e7e276c1eee3443d450557f8aba664aa4b1eb0
MaysaraSoliman <144054764+MaysaraSoliman@users.noreply.github.com>
2023-11-26 03:23:42 +0200

    add files

---
+++ safety&fire equipments website/Assets/Scripts/global.js
@@ -0,0 +1,9 @@
+// Header Scroll
+const header = document.querySelector("header");
+
+const headerChangeColor = function () {
+    window.scrollY > 50
+        ? header.classList.add("scroll")
+        : header.classList.remove("scroll");
+};
+window.addEventListener("scroll", headerChangeColor);
