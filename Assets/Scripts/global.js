61b11b439841ce74e7821b58250803daceb1a48e
MaysaraSoliman <144054764+MaysaraSoliman@users.noreply.github.com>
2023-11-28 12:55:31 +0200

    Update global.js

--- Assets/Scripts/global.js
+++ Assets/Scripts/global.js
@@ -11,15 +11,16 @@ window.addEventListener("scroll", headerChangeColor);
 // ts-features section
 // ts-features tabs
 
-let navTabs = document.querySelectorAll("#ts-features .featured-tab .nav-tabs li");
+let navTabsLi = document.querySelectorAll("#ts-features .featured-tab .nav-tabs li ");
+let navTabs = document.querySelectorAll("#ts-features .featured-tab .nav-tabs li a");
 
 function handleActiveClass() {
     navTabs.forEach(tab => {
         tab.addEventListener("click", function () {
             navTabs.forEach(tab => {
-                tab.classList.remove("active");
+                tab.closest("li").classList.remove("active");
             })
-            this.classList.add("active");
+            this.closest("li").classList.add("active");
         })
     })
 }
